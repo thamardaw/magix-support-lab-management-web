1eb07935c099a86c9d2445611123d3213ae37755
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-04-28 11:59:27 +0630

    added recoil state management

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import { Login, ResetPassword, Signup } from "./containers";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <RecoilRoot>
+      <Router>
+        <Switch>
+          {/* <Route path="/dashboard" component={Dashboard} /> */}
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/resetPassword" component={ResetPassword} />
+          <Redirect to="/login" />
+        </Switch>
+      </Router>
+    </RecoilRoot>
+  );
 }
 
 export default App;
