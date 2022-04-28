69830154fcab2cc91197050d22d765a02332cd53
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-04-28 17:48:21 +0630

    added private route

---
+++ src/hocs/PrivateRoute.js
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import authAtom from "../recoil/auth";
+
+function PrivateRoute({ component: Component }) {
+  const auth = useRecoilValue(authAtom);
+  if (auth) {
+    return <Component />;
+  } else {
+    return <Navigate to="/login" replace />;
+  }
+}
+
+export default PrivateRoute;
