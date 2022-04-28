ecae1d3ac2f58d58faf0b9bfc0808ff96854f03a
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-04-28 18:32:56 +0630

    added main containers

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,4 @@
 export { default as Login } from "./Login";
 export { default as Signup } from "./Signup";
 export { default as ResetPassword } from "./ResetPassword";
+export { default as Dashboard } from "./Dashboard";
