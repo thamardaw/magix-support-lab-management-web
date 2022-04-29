946ac9fe1fc78fa12314f70cad4a9d2314d66ab9
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-04-29 14:58:30 +0630

    added appbar, drawer and footer on dashboard

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,20 +1,41 @@
-import { useSetRecoilState } from "recoil";
-import { useNavigate } from "react-router-dom";
-import authAtom from "../recoil/auth";
+import { Box, CssBaseline, Toolbar } from "@mui/material";
+import { useState } from "react";
+import { Routes } from "react-router-dom";
+import { Appbar, Footer, ResponsiveDrawer } from "../components";
+
+const drawerWidth = 240;
 
 const Dashboard = () => {
-  const navigate = useNavigate();
-  const setAuth = useSetRecoilState(authAtom);
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
   return (
-    <button
-      onClick={() => {
-        setAuth(null);
-        localStorage.removeItem("magix-support-auth-tokens");
-        navigate("/");
-      }}
-    >
-      logout
-    </button>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <Appbar
+        drawerWidth={drawerWidth}
+        handleDrawerToggle={handleDrawerToggle}
+      />
+      <ResponsiveDrawer
+        drawerWidth={drawerWidth}
+        mobileOpen={mobileOpen}
+        handleDrawerToggle={handleDrawerToggle}
+      />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+        <Routes></Routes>
+        <Footer />
+      </Box>
+    </Box>
   );
 };
 
