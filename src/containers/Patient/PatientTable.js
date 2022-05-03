70384d556c13e9aa20fc6886a0a5291cedc0f911
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-05-03 22:04:55 +0630

    added toolbar buttons actions

--- src/containers/Patient/PatientTable.js
+++ src/containers/Patient/PatientTable.js
@@ -1,66 +1,97 @@
 import { Button } from "@mui/material";
+import { memo } from "react";
 import { CustomTable } from "../../components";
+import { useNavigate } from "react-router-dom";
 
-function createData(name, calories, fat, carbs, protein) {
+function createData(
+  id,
+  name,
+  age,
+  contact_details,
+  gender,
+  date_of_birth,
+  address,
+  created_time
+) {
   return {
+    id,
     name,
-    calories,
-    fat,
-    carbs,
-    protein,
+    age,
+    contact_details,
+    gender,
+    date_of_birth,
+    address,
+    created_time,
   };
 }
 
 const rows = [
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Donut", 452, 25.0, 51, 4.9),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Honeycomb", 408, 3.2, 87, 6.5),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Jelly Bean", 375, 0.0, 94, 0.0),
-  createData("KitKat", 518, 26.0, 65, 7.0),
-  createData("Lollipop", 392, 0.2, 98, 0.0),
-  createData("Marshmallow", 318, 0, 81, 2.0),
-  createData("Nougat", 360, 19.0, 9, 37.0),
-  createData("Oreo", 437, 18.0, 63, 4.0),
+  createData(
+    1,
+    "Aung Aung",
+    "18",
+    "09123456789",
+    "male",
+    "2002-2-2",
+    "Kamaryut",
+    "2022-04-24 11:11 AM"
+  ),
 ];
 
 const headCells = [
   {
-    id: "name",
+    id: "id",
     numeric: false,
     disablePadding: true,
-    label: "Dessert (100g serving)",
+    label: "ID",
+    minWidth: 50,
   },
   {
-    id: "calories",
+    id: "name",
     numeric: false,
     disablePadding: false,
-    label: "Calories",
+    label: "Name",
   },
   {
-    id: "fat",
+    id: "age",
     numeric: false,
     disablePadding: false,
-    label: "Fat (g)",
+    label: "Age",
   },
   {
-    id: "carbs",
+    id: "contact_details",
     numeric: false,
     disablePadding: false,
-    label: "Carbs (g)",
+    label: "Contact Details",
   },
   {
-    id: "protein",
+    id: "gender",
     numeric: false,
     disablePadding: false,
-    label: "Protein (g)",
+    label: "Gender",
+  },
+  {
+    id: "date_of_birth",
+    numeric: false,
+    disablePadding: false,
+    label: "Date of Birth",
+  },
+  {
+    id: "address",
+    numeric: false,
+    disablePadding: false,
+    label: "Address",
+  },
+  {
+    id: "created_time",
+    numeric: false,
+    disablePadding: false,
+    label: "Date & Time",
   },
 ];
 
 const PatientTable = () => {
+  const navigate = useNavigate();
   return (
     <CustomTable
       tableConfig={{
@@ -69,35 +100,36 @@ const PatientTable = () => {
         maxHeight: "62vh",
       }}
       data={rows}
+      isLoading={false}
       toolbarButtons={{
         whenNoneSelected: [
           {
             id: "patient table new button",
-            component: ({ ...rest }) => (
+            component: memo(({ ...rest }) => (
               <Button variant="outlined" size="small" {...rest}>
                 New
               </Button>
-            ),
+            )),
             callback: (selected) => {
-              console.log(selected);
+              navigate("form");
             },
           },
         ],
         whenOneSelected: [
           {
             id: "patient table edit button",
-            component: ({ ...rest }) => (
+            component: memo(({ ...rest }) => (
               <Button variant="contained" size="small" {...rest}>
                 Edit
               </Button>
-            ),
+            )),
             callback: (selected) => {
-              console.log(selected);
+              navigate("form/1");
             },
           },
           {
             id: "patient table detail button",
-            component: ({ ...rest }) => (
+            component: memo(({ ...rest }) => (
               <Button
                 variant="contained"
                 size="small"
@@ -106,14 +138,14 @@ const PatientTable = () => {
               >
                 Details
               </Button>
-            ),
+            )),
             callback: (selected) => {
-              console.log(selected);
+              navigate("details/1");
             },
           },
           {
             id: "patient table delete button",
-            component: ({ ...rest }) => (
+            component: memo(({ ...rest }) => (
               <Button
                 variant="contained"
                 size="small"
@@ -123,7 +155,7 @@ const PatientTable = () => {
               >
                 Delete
               </Button>
-            ),
+            )),
             callback: (selected) => {
               console.log(selected);
             },
