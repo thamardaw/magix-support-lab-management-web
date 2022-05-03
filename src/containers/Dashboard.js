c4e9da709e2cf7d1e5a3c14c5c2529f9ed5523ad
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-05-04 01:18:07 +0630

    registered lab test and lab report route

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,8 +1,10 @@
 import { Box, CssBaseline, Toolbar } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Appbar, Footer, ResponsiveDrawer } from "../components";
+import { LabTest } from "./LabTest";
 import { Patient } from "./Patient";
 import { TestCategory } from "./TestCategory";
+import { LabReport } from "./LabReport";
 
 const drawerWidth = 240;
 
@@ -24,6 +26,8 @@ const Dashboard = () => {
         <Routes>
           <Route path={`patient/*`} element={<Patient />} />
           <Route path={`test_category/*`} element={<TestCategory />} />
+          <Route path={`lab_test/*`} element={<LabTest />} />
+          <Route path={`lab_report/*`} element={<LabReport />} />
           <Route path="*" element={<Navigate to="patient" replace />} />
         </Routes>
         <Footer />
