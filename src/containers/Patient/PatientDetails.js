6fa4c1e0218315a6df0bcd6b5b8a0ed87132df30
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-05-03 22:04:23 +0630

    added uis for patient detials and patient form

--- src/containers/Patient/PatientDetails.js
+++ src/containers/Patient/PatientDetails.js
@@ -1,5 +1,34 @@
+import { Box, Divider, Toolbar, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { BackButton, DetailsRow } from "../../components";
+
 const PatientDetails = () => {
-  return <h1>Patient Details</h1>;
+  const navigate = useNavigate();
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Toolbar
+        sx={{
+          display: "flex",
+          paddingLeft: "12px",
+        }}
+        variant="dense"
+        disableGutters={true}
+      >
+        <BackButton backFunction={() => navigate(-1)} />
+        <Typography variant="h5">Details</Typography>
+      </Toolbar>
+      <Divider />
+      <Box sx={{ flexDirection: "column", padding: "10px" }}>
+        <DetailsRow name="ID" value="1" />
+        <DetailsRow name="Name" value="Aung Aung" />
+        <DetailsRow name="Age" value="18" />
+        <DetailsRow name="Contact Details" value="09123456789" />
+        <DetailsRow name="Gender" value="male" />
+        <DetailsRow name="Date Of Birth" value="2002-2-2" />
+        <DetailsRow name="Address" value="Kamaryut" />
+      </Box>
+    </Box>
+  );
 };
 
 export default PatientDetails;
--- src/containers/Patient/PatientForm.js
+++ src/containers/Patient/PatientForm.js
@@ -1,8 +1,196 @@
-import { useParams } from "react-router-dom";
+import { LoadingButton } from "@mui/lab";
+import {
+  Box,
+  Divider,
+  MenuItem,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import BackButton from "../../components/BackButton";
 
 const PatientForm = () => {
   const { id } = useParams();
-  return <h1>Patient Form {id}</h1>;
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [details, setDetails] = useState({
+    name: "",
+    age: "",
+    contact_details: "",
+    gender: "",
+    date_of_birth: null,
+    address: "",
+  });
+
+  const handleChange = (e) => {
+    setDetails({ ...details, [e.target.name]: e.target.value });
+  };
+
+  const createNew = async () => {};
+  const update = async () => {};
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Toolbar
+        sx={{
+          display: "flex",
+          paddingLeft: "12px",
+        }}
+        variant="dense"
+        disableGutters={true}
+      >
+        <BackButton backFunction={() => navigate(-1)} />
+        <Typography variant="h5">{id ? "Edit" : "New"}</Typography>
+      </Toolbar>
+      <Divider />
+      <Box sx={{ flexDirection: "column", padding: "10px" }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ width: "30%" }}>
+            <Typography variant="p">Name</Typography>
+          </Box>
+          <TextField
+            size="small"
+            sx={{ width: "70%" }}
+            margin="dense"
+            value={details?.name || ""}
+            name="name"
+            onChange={handleChange}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ width: "30%" }}>
+            <Typography variant="p">Age</Typography>
+          </Box>
+          <TextField
+            size="small"
+            sx={{ width: "70%" }}
+            margin="dense"
+            value={details?.age || ""}
+            name="age"
+            onChange={handleChange}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ width: "30%" }}>
+            <Typography variant="p">Contact Details</Typography>
+          </Box>
+          <TextField
+            size="small"
+            sx={{ width: "70%" }}
+            margin="dense"
+            value={details?.contact_details || ""}
+            name="contact_details"
+            onChange={handleChange}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ width: "30%" }}>
+            <Typography variant="p">Gender</Typography>
+          </Box>
+          {/* <TextField size="small" sx={{ width: "70%" }} margin="dense" /> */}
+          <TextField
+            select
+            fullWidth
+            label="Gender"
+            size="small"
+            sx={{ width: "70%" }}
+            margin="dense"
+            value={details?.gender || ""}
+            name="gender"
+            onChange={handleChange}
+          >
+            <MenuItem value="male">Male</MenuItem>
+            <MenuItem value="female">Female</MenuItem>
+          </TextField>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ width: "30%" }}>
+            <Typography variant="p">Date Of Birth</Typography>
+          </Box>
+          <TextField
+            size="small"
+            sx={{ width: "70%" }}
+            margin="dense"
+            placeholder="YYYY-MM-DD"
+            value={details?.date_of_birth || ""}
+            name="date_of_birth"
+            onChange={handleChange}
+          />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Box sx={{ width: "30%" }}>
+            <Typography variant="p">Address</Typography>
+          </Box>
+          <TextField
+            size="small"
+            sx={{ width: "70%" }}
+            margin="dense"
+            value={details?.address || ""}
+            name="address"
+            onChange={handleChange}
+          />
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "flex-end",
+          padding: "10px",
+        }}
+      >
+        <LoadingButton
+          variant="contained"
+          loading={isLoading}
+          size="small"
+          sx={{ marginRight: "5px" }}
+          onClick={id ? update : createNew}
+        >
+          Save
+        </LoadingButton>
+      </Box>
+    </Box>
+  );
 };
 
 export default PatientForm;
