1678ccae14d26a23a4960c5475f78aab85e25c68
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-05-03 22:03:24 +0630

    added details row and backbutton export

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export { default as ResponsiveDrawer } from "./ResponsiveDrawer";
 export { default as Appbar } from "./Appbar";
 export { default as Footer } from "./Footer";
 export { default as CustomTable } from "./CustomTable";
+export { default as BackButton } from "./BackButton";
+export { default as DetailsRow } from "./DetailsRow";
