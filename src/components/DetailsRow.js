ddb97fb7e6be02845cc461784789407126349d36
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-05-03 22:02:59 +0630

    added details row

---
+++ src/components/DetailsRow.js
@@ -0,0 +1,51 @@
+import PropTypes from "prop-types";
+import { alpha, Box, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+const StyledBox = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  margin: "10px 0px",
+  padding: "10px",
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.primary.light, 0.1),
+  },
+}));
+
+const DetailsRow = ({
+  leftWidth = "30%",
+  rightWidth = "70%",
+  textVariant = "body2",
+  name,
+  value,
+}) => {
+  return (
+    <StyledBox>
+      <Box sx={{ width: leftWidth }}>
+        <Typography variant={textVariant} sx={{ fontWeight: "bold" }}>
+          {name}
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          width: `calc(${rightWidth} - 5px)`,
+          marginLeft: "5px",
+          wordWrap: "break-word",
+        }}
+      >
+        <Typography variant={textVariant}>{value}</Typography>
+      </Box>
+    </StyledBox>
+  );
+};
+
+DetailsRow.propTypes = {
+  leftWidth: PropTypes.string,
+  rightWidth: PropTypes.string,
+  textVariant: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.any,
+};
+
+export default DetailsRow;
