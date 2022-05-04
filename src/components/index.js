cb74b1359c864f19a4b3421acdbc91f112454a4d
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-05-04 10:56:21 +0630

    added delete dialog componenet

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Footer } from "./Footer";
 export { default as CustomTable } from "./CustomTable";
 export { default as BackButton } from "./BackButton";
 export { default as DetailsRow } from "./DetailsRow";
+export { default as DeleteDialog } from "./DeleteDialog";
