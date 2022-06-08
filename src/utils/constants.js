d04dcb031d25973d07059581067dc26836663a65
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-06-08 10:51:48 +0630

    Minor lab result upgrade (#15)
    * added parameter form"
    
    * integrated parameter form with api
    
    * minor fix and added delete button in parameter
    
    * integrated api to lab test details
    
    * integrated api to lab report api
    
    * integrated lab report with api
    
    * fix(auth): wrapped dashboard in auth
    
    * style: changed btn icon to details view
    
    * style(forms): added tab to only form
    
    before the tabs are headertab which have header form and lab result form which have lab result sub form and preview in it. now the tabs are on forms level
    
    * fix: changed screen flow
    
    * fix(result_default_text): make sure not to split an array
    
    * feat(patient): added id format
    
    * fix(lab_report_sub_form): make sure the form reset
    
    * fix(lab_report_sub_form): make sure form reset
    
    * fix(custom_table): changed table default row to 25
    
    * fix(lab_test_sub_form): updated the request model according to backend change
    
    * fix(lab_test_details): changed the key name
    
    * fix(parameter_form): take save button out of the scroll area
    
    * feat(parameter_form): added a check button
    
    the check button is meant for show in report. Whether to show title in report form.
    
    * feat(lab_result): lab result's test name will be show according to user choice
    
    * feat(lab_result): added header
    
    * feat(details_screen): added edit and delete button in details page
    
    * fix: removed unnecessary file
    
    * fix(lab_result_form): spelling fix
    
    * fix: minor
    
    * feat: updated env
    
    * fix: changes letter head language to burmese

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,16 +1,25 @@
 export const constants = {
   name: process.env.REACT_APP_TITLE || "Magix Support",
+  name_bur: process.env.REACT_APP_TITLE_BUR || "Magix Support",
   hospital_name: `${process.env.REACT_APP_TITLE || "Magix Support"} Hospital`,
   hospital_name_symbol: process.env.REACT_APP_TITLE_SHORT || "MS",
   hospital_logo: process.env.REACT_APP_LOGO,
   hospital_desc:
     process.env.REACT_APP_DESC || "Specialist Clinic and Diagnostic Center",
+  hospital_desc_bur:
+    process.env.REACT_APP_DESC_BUR ||
+    "အထူးကုဆေးခန်း နှင့် အဆင့်မြင့်ရောဂါရှာဖွေရေးစင်တာ",
   primary_color: process.env.REACT_APP_THEME || "#1976d2",
   hospital_phones:
     process.env.REACT_APP_PHONES || "09-97-8396650, 09-94-3031598 (Viber)",
+  hospital_phones_bur:
+    process.env.REACT_APP_PHONES_BUR || "၀၉-၉၇-၈၃၉၆၆၅၀၊ ၀၉-၉၄-၃၀၃၁၅၉၈",
   hospital_address:
     process.env.REACT_APP_ADDRESS ||
     "No.3 A, Kan Taw Mon Housing, Lay Daung Kan Road, Thingangyun township (Near May Ka Housing)",
+  hospital_address_bur:
+    process.env.REACT_APP_ADDRESS_BUR ||
+    "အမှတ် ၃ (အေ)၊ ကန်တော်မွန်အိမ်ရာ၊ လေးထောင့်ကန်လမ်း (မေခအိမ်ရာအနီး)၊ သင်္ဃန်းကျွန်းမြို့နယ်။",
   hospital_email: process.env.REACT_APP_EMAILS || "kantawmon.clinic@gmail.com",
   hospital_fb:
     process.env.REACT_APP_FB ||
